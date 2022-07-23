eb2bf14a5bf1859e7a64ea3808cf6108466c3142
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 22:00:45 +0600

    database connection setup done

---
+++ database/db.js
@@ -0,0 +1,8 @@
+export const Connection = () => {
+	try {
+	} catch (error) {
+		console.log('Error while connecting to Database', error.message)
+	}
+}
+
+export default Connection
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require('express')
+// const express = require('express')
+// const Connection = require('./database/db')
+import express from 'express'
+import Connection from './database/db.js'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
+Connection()
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
 })
+
+// user
+// FUwAeq2mz6hGn31c
