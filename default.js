8ea2d8dfb30a10bdaaffa1b9a4d079fc7337e07b
khanshorif331 <khanshorif331@gmail.com>
2022-07-24 00:55:10 +0600

    new data updated in the db and fixed some bugs

--- default.js
+++ default.js
@@ -1,9 +1,9 @@
 import { products } from './constants/data.js'
 import product from './model/product-schema.js'
 
-const DefaultData = () => {
+const DefaultData = async () => {
 	try {
-		product.insertMany(products)
+		await product.insertMany(products)
 		console.log('Default data inserted successfully')
 	} catch (error) {
 		console.log('Error while inserting default data', error.message)
