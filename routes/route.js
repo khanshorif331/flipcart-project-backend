a6869e80325b8fe73c7cfce6b8be2f0bfd7d492d
khanshorif331 <khanshorif331@gmail.com>
2022-08-08 01:31:02 +0600

    router created for get produt by id

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { userSignup, userLogin } from '../controller/user-controller.js'
-import { getProducts } from '../controller/product-controller.js'
+import {
+	getProducts,
+	getProductById,
+} from '../controller/product-controller.js'
 
 const router = express.Router()
 
@@ -12,5 +15,6 @@ router.post('/signup', userSignup)
 router.post('/login', userLogin)
 
 router.get('/products', getProducts)
+router.get('/product/:id', getProductById)
 
 export default router
