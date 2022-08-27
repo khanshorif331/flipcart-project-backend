75cadc3a6bd0deed53fd02e173e6f8786607a273
khanshorif331 <khanshorif331@gmail.com>
2022-08-27 19:18:22 +0600

    fixed some bug of the login/signup api

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -3,19 +3,21 @@ import User from '../model/user-schema.js'
 export const userSignup = async (req, res) => {
 	try {
 		const exist = await User.findOne({ username: req.body.username })
-		console.log(exist)
+		// console.log(exist)
 		if (exist) {
 			return res.status(401).json({
 				message: 'Username already exists',
 			})
 		}
 		const user = req.body
-		console.log(user, 'user')
+		// console.log(user, 'user')
 		const newUser = new User(user)
 		await newUser.save()
 		res.status(200).json({ message: 'User created successfully', user })
 	} catch (err) {
-		res.status(500).json({ message: err.message })
+		res.status(500).json({
+			message: err.message,
+		})
 	}
 }
 
